e491d6186efef07735ef97b53fa0f2121727a0fa
AlisonCardoso <alisoncardoso7@gmail.com>
2024-10-10 15:55:31 -0300

    formulario de cadastro de oficinas

--- teste-vite/resources/views/livewire/buscar-cnpj.blade.php
+++ teste-vite/resources/views/livewire/buscar-cnpj.blade.php
@@ -15,12 +15,13 @@
             @enderror
         </div>
         <div class="mb-4">
-            <label for="nome_fantasia" class="block text-gray-300 text-sm font-bold mb-2">Nome Fantasia</label>
-            <input type="text" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-700 text-white" id="nome_fantasia" wire:model="nome_fantasia">
-            @error('nome_fantasia')
+            <label for="cnae_fiscal_descricao" class="block text-gray-300 text-sm font-bold mb-2">Nome Fantasia</label>
+            <input type="text" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-700 text-white" id="cnae_fiscal_descricao" wire:model="cnae_fiscal_descricao">
+            @error('cnae_fiscal_descricao')
             <span class="badge bg-warning">{{ $message }}</span>
             @enderror
         </div>
+       
         <div class="mb-4">
             <label for="descricao_situacao_cadastral" class="block text-gray-300 text-sm font-bold mb-2">Situação</label>
             <input type="text" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-700 text-white" id="descricao_situacao_cadastral" wire:model="descricao_situacao_cadastral">
@@ -28,23 +29,7 @@
             <span class="badge bg-warning">{{ $message }}</span>
             @enderror
         </div>
+   
         
-        <div class="mb-6">
-            <label for="cnae_fiscal_descricao" class="block text-gray-300 text-sm font-bold mb-2">Descrição</label>
-            <textarea rows="4" placeholder="digite um descrição (opcional)" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-700 text-white"
-            id="cnae_fiscal_descricao" wire:model="cnae_fiscal_descricao">
-            @error('cnae_fiscal_descricao')
-            <span class="badge bg-warning">{{ $message }}</span>
-            @enderror
-        </div>
-        <div>
-            <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
-             type="button" wire:click="save" >Salvar Empresa</button>
-        </div>
-        <div>
-        <button class="bg-red-500 text-white px-4 py-2 rounded-md hover
-        :focus:shadow-outline-blue" type="button" wire:click="cancel">Cancelar</
-        button>
-        </div>
 </div>
 
