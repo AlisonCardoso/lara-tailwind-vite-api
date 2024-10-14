c04a1687774d44ed8f5122ecee0294922e4deef1
AlisonCardoso <alisoncardoso7@gmail.com>
2024-10-14 17:22:20 -0300

    limpando dadoss

--- teste-vite/resources/views/livewire/buscar-cnpj.blade.php
+++ teste-vite/resources/views/livewire/buscar-cnpj.blade.php
@@ -23,49 +23,6 @@
             <input type="text" id="descricao_situacao_cadastral" name="descricao_situacao_cadastral" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('descricao_situacao_cadastral', $edit->descricao_situacao_cadastral ?? '') }}" wire:model="descricao_situacao_cadastral">
             @error('descricao_situacao_cadastral') <span class="text-red-500">{{ $message }}</span> @enderror
         </div>
-    </div>
-
-
-
-
-
-
-
-
-
-{{-- 
-
-        <div class="mb-4">
-            <label for="cnpj" class="block text-gray-300 text-sm font-bold mb-2" :value="__('CNPJ') " >CNPJ</label>
-            <input type="text" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-700 text-white" id="cnpj"  name="cnpj" :value="old('cnpj')" wire:model.lazy="cnpj" placeholder="Insira o CNPJ (Apenas números)" />
-            @error('cnpj')
-            <span class="bg-red-500 text-white rounded-md p-1">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-4">
-            <label for="razao_social" class="block text-gray-300 text-sm font-bold mb-2">Razão Social</label>
-            <input type="text" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-700 text-white" id="razao_social"  name="razao_social" :value="old('razao_social')" wire:model="razao_social">
-            @error('razao_social')
-            <span class="bg-red-500 text-white rounded-md p-1">{{ $message }}</span>
-            @enderror
-        </div>
-       
-        <div class="mb-4">
-            <label for="cnae_fiscal_descricao" class="block text-gray-300 text-sm font-bold mb-2">Nome Fantasia</label>
-            <input type="text" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-700 text-white" id="cnae_fiscal_descricao"  name="cnae_fiscal_descricao" :value="old('cnae_fiscal_descricao')"  wire:model="cnae_fiscal_descricao">
-            @error('cnae_fiscal_descricao')
-            <span class="bg-red-500 text-white rounded-md p-1">{{ $message }}</span>
-            @enderror
-        </div> 
-        <div class="mb-4">
-            <label for="descricao_situacao_cadastral" class="block text-gray-300 text-sm font-bold mb-2">Situação</label>
-            <input type="text" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 bg-gray-700 text-white" id="descricao_situacao_cadastral"  name="descricao_situacao_cadastral" :value="old('descricao_situacao_cadastral')" wire:model="descricao_situacao_cadastral">
-            @error('descricao_situacao_cadastral')
-            <span class="bg-red-500 text-white rounded-md p-1">{{ $message }}</span>
-            @enderror
-        </div>
-         --}}
-      
-        
+    </div> 
 </div>
 
--- teste-vite/resources/views/workshop/create.blade.php
+++ teste-vite/resources/views/workshop/create.blade.php
@@ -12,93 +12,11 @@
         <form method="POST" action="{{ isset($edit) ? route('workshops.update', ['id' => $edit->id]) : route('workshops.store') }}" enctype="multipart/form-data">
             @csrf
             @if(isset($edit)) @method('PUT') @endif
- @livewire('buscar-cnpj')
+ 
+ 
+            @livewire('buscar-cnpj')
 
-            {{--  
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
-                <div>
-                    <label for="cnpj" class="block text-gray-800 dark:text-gray-300">CNPJ</label>
-                    <input type="text" id="cnpj" name="cnpj" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('cnpj', $edit->cnpj ?? '') }}" required>
-                    @error('cnpj') <span class="text-red-500">{{ $message }}</span> @enderror
-                </div>
-                <div>
-                    <label for="razao_social" class="block text-gray-800 dark:text-gray-300">Razão Social</label>
-                    <input type="text" id="razao_social" name="razao_social" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('razao_social', $edit->razao_social ?? '') }}" required>
-                    @error('razao_social') <span class="text-red-500">{{ $message }}</span> @enderror
-                </div>
-            </div>
-
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
-                <div>
-                    <label for="cnae_fiscal_descricao" class="block text-gray-800 dark:text-gray-300">Nome Fantasia</label>
-                    <input type="text" id="cnae_fiscal_descricao" name="cnae_fiscal_descricao" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('cnae_fiscal_descricao', $edit->cnae_fiscal_descricao ?? '') }}">
-                    @error('cnae_fiscal_descricao') <span class="text-red-500">{{ $message }}</span> @enderror
-                </div>
-                <div>
-                    <label for="descricao_situacao_cadastral" class="block text-gray-800 dark:text-gray-300">Situação</label>
-                    <input type="text" id="descricao_situacao_cadastral" name="descricao_situacao_cadastral" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('descricao_situacao_cadastral', $edit->descricao_situacao_cadastral ?? '') }}">
-                    @error('descricao_situacao_cadastral') <span class="text-red-500">{{ $message }}</span> @enderror
-                </div>
-            </div>
-            --}}
-
-            <div class="mb-6">
-                <label for="email" class="block text-gray-800 dark:text-gray-300">E-mail</label>
-                <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('email', $edit->email ?? '') }}" required>
-                @error('email') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6"> 
-            <div class="mb-6">
-                <label for="responsavel" class="block text-gray-800 dark:text-gray-300">Nome do Responsável</label>
-                <input type="text" id="responsavel" name="responsavel" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('responsavel', $edit->responsavel ?? '') }}" required>
-                @error('responsavel') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="mb-6">
-                <label for="phone_number" class="block text-gray-800 dark:text-gray-300">Telefone</label>
-                <input type="text" id="phone_number" name="phone_number" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('phone_number', $edit->phone_number ?? '') }}" required>
-                @error('phone_number') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-        </div>
-
-@livewire('buscar-cep')
-
-        {{-- 
-
-          <div class="grid grid-cols-1 sm:grid-cols-3 gap-6 mb-6">
-            <div class="mb-6">
-                <label for="cep" class="block text-gray-800 dark:text-gray-300">CEP</label>
-                <input type="text" id="cep" name="cep" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('cep', $edit->address->cep ?? '') }}" required>
-                @error('cep') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="mb-6">
-                <label for="state" class="block text-gray-800 dark:text-gray-300">Estado</label>
-                <input type="text" id="state" name="state" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('state', $edit->address->state ?? '') }}" required>
-                @error('state') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="mb-6">
-                <label for="city" class="block text-gray-800 dark:text-gray-300">Cidade</label>
-                <input type="text" id="city" name="city" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('city', $edit->address->city ?? '') }}" required>
-                @error('city') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-          </div>
-
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
-                <div>
-                <label for="neighborhood" class="block text-gray-800 dark:text-gray-300">Bairro</label>
-                <input type="text" id="neighborhood" name="neighborhood" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('neighborhood', $edit->address->neighborhood ?? '') }}" required>
-                @error('neighborhood') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-
-            <div>
-                <label for="street" class="block text-gray-800 dark:text-gray-300">Rua</label>
-                <input type="text" id="street" name="street" class="w-full px-3 py-2 border rounded-md bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white focus:outline-none focus:ring focus:ring-blue-500" value="{{ old('street', $edit->address->street ?? '') }}" required>
-                @error('street') <span class="text-red-500">{{ $message }}</span> @enderror
-            </div>
-            </div>
-             --}}
+            {{--  aqui serao inseridos os inputs do livewire--}}
 
             <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
                 <div>
