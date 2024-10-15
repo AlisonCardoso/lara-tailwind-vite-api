c667d0887d15a87be0f6767da67958787553bd08
AlisonCardoso <alisoncardoso7@gmail.com>
2024-10-15 10:42:21 -0300

    create crpm, bpm cias e seeders

--- teste-vite/database/seeders/DatabaseSeeder.php
+++ teste-vite/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\RegionalCommand;
+use App\Models\SubCommand;
+use App\Models\Company;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,5 +23,24 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+           // SituacaoContaSeeder::class,
+           // ContaSeeder::class,
+         // TipoContaSeeder::class,
+
+           SeederComandoRegional::class,
+           SeederBatalhao::class,
+           SeederCompanies::class,
+          //  SeederType_vehicle::class,
+         
+           
+             // CategorySeeder::class,
+             // SubcategorySeeder::class,
+             // SituationVehiclesSeeder::class,
+             
+            
+        ]);
+
     }
 }
